dc8f23e95e31948a33b7ab44a0938f0aa9c66da7
Alaa Mensh <princesslole464@yahoo.com>
2021-04-20 23:26:40 +0200

    done first Pation regisration

--- Router/pation.js
+++ Router/pation.js
@@ -5,5 +5,10 @@ app.use(bodyParser);
 const db = require('../dataBase/dataBaseConnection');
 
 router.post('/addpt', async function(req,res){
-   console.log(req.body);
+    var result=JSON.stringify(req.body);  
+    var json =  JSON.parse(result);  
+   // console.log('asds',json.Problem);
+   console.log(json.familyHistory[0].problem);
 });
+
+module.exports = router;
