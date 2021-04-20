176f963b07a09d0cc318dff8380f88e9646f5f6e
unknown <abdo_201333@hotmail.com>
2021-04-20 23:44:44 +0200

    add pt

--- Router/pation.js
+++ Router/pation.js
@@ -7,8 +7,26 @@ const db = require('../dataBase/dataBaseConnection');
 router.post('/addpt', async function(req,res){
     var result=JSON.stringify(req.body);  
     var json =  JSON.parse(result);  
-   // console.log('asds',json.Problem);
    console.log(json.familyHistory[0].problem);
+   db.query('INSERT INTO ' + '`Patient`'+  '(phone,address,	email,birthDate,maritalStatus,bloodGroup,firstName,lastName) VALUES('+'"'+json.phone+'"'+','+'"'+json.address+'"'+','+'"'+json.email+'"'+','+'"'+json.birthDate+'"'+','+
+   '"'+json.status+'"'+','+'"'+ json.BloodGroup +'"' + ',' +'"'+ json.firstName +'"' +','+ '"'+json.lastName +'"'+');', function (err, result) {
+   if (err) {
+   console.log("err=>>" + err);
+   res.send(err);
+   }else{
+      console.log(result[0]["id"])
+      for(var i =0;i<json.Allergy.length;i++){
+         db.query('INSERT INTO ' + '`pt_allergy`'+  '(type,reaction,	notes,ptid) VALUES('+'"'+json.Allergy[i].type+'"'+','+'"'+json.Allergy[i].reaction+'"'+','+'"'+json.Allergy[i].notes+'"'+1+');', function (err, result) {
+   if (err) {
+      res.send(err); 
+   }else{
+      res.send("done")
+   }
+      })
+   }
+
+   }
+        })
 });
 
 module.exports = router;
