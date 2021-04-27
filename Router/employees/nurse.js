e14fcd2dc5ec08fe239ffb19988af4d248f17e2d
unknown <princesslole464@yahoo.com>
2021-04-28 01:11:44 +0200

    edit nurse.js fix some bugs

--- Router/employees/nurse.js
+++ Router/employees/nurse.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const modifyFunction = require('../requestsModiy');
 app.use(bodyParser);
 const db = require('../../dataBase/dataBaseConnection');
-
+/*
+ id:-1,
+            date: this.getDate(),
+            time: "4:23",
+            temp:this.state.temp,
+            pulse: this.state.pulse,
+            bloodPressure: this.state.bloodPressure,
+            respiratoryRate: this.state.respiratoryRate ,
+            OXSat: this.state.oxygenSaturation,
+            height: this.state.height,
+            weight: this.state.weight,
+            BMI: this.state.BMI,
+            pain: this.state.pain,
+            smokingStatus: this.state.smokingStatus,
+            headC: this.state.headCircumference,
+ */
 
 router.post('/addNurse', async function(req,res){
     let a = db.query('INSERT INTO `nurse` (firstName, lastName, Email,degree,address,phone,userName,password , Date) VALUES  (' +'"'+ req.body.firstName +'"'+ 
@@ -54,7 +70,7 @@ router.get('/getNurse',async function(req,res){
 
 })
 
-  router.delete('/deleteDoctor',async function(req,res){
+  router.delete('/deleteNurse',async function(req,res){
     db.query('DELETE  FROM `nurse` where id = ' + req.body.id,function(err,result){
         if(err){
             res.send(err);
